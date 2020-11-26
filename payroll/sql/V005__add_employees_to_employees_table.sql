7aacbc55638273c3fb869cebb532d548577ae5a9
Tpolm <rdt.project@gmail.com>
2020-11-26 22:42:25 +0100

    Add employees to employees table

---
+++ payroll/sql/V005__add_employees_to_employees_table.sql
@@ -0,0 +1,2 @@
+INSERT INTO `employees` (`id`, `first_name`, `last_name`, `title_id`, `phone_number`) VALUES ('1', 'Wes', 'Higbee', '1', '23456734');
+INSERT INTO `employees` (`id`, `first_name`, `last_name`, `title_id`, `phone_number`) VALUES ('2', 'John', 'Snow', '2', '23452345');
